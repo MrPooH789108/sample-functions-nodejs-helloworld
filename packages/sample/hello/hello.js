358f142517a3c2ec930a4d3f0516e4606c852b99
MrPooH789108 <wasin.s@bluegreen.co.th>
2025-05-27 10:41:37 +0700

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -2,6 +2,7 @@ function main(args) {
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
+    console.log(process.env.DD_API_KEY)
     return {"body": greeting}
   }
 
